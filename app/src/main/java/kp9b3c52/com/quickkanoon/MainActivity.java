0c5f63ff68b1a929acdc516e5a8a3d0b0f4ccc75
anubrata <anubrata.stephenite.nrg@gmail.com>
2017-09-05 23:36:59 +0530

    Initial commit

---
+++ app/src/main/java/kp9b3c52/com/quickkanoon/MainActivity.java
@@ -0,0 +1,13 @@
+package kp9b3c52.com.quickkanoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
