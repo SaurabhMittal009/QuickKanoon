b55cbc038faef18f6ee2bf0094c150e311aec23b
anubrata <20087176+AnubrataDS@users.noreply.github.com>
2017-09-07 01:13:58 +0530

    Basic Front Page , back end work begins now

--- app/src/main/java/kp9b3c52/com/quickkanoon/MainActivity.java
+++ app/src/main/java/kp9b3c52/com/quickkanoon/MainActivity.java
@@ -1,15 +1,16 @@
 package kp9b3c52.com.quickkanoon;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view)
     {
-        Log.i("Info","Button clicked");
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
