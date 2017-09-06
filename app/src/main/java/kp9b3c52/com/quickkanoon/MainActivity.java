1ad5575dee713e0ee0f494138b579b1435d95cc8
anubrata <20087176+AnubrataDS@users.noreply.github.com>
2017-09-06 20:52:41 +0530

    test commit to check if everything works

--- app/src/main/java/kp9b3c52/com/quickkanoon/MainActivity.java
+++ app/src/main/java/kp9b3c52/com/quickkanoon/MainActivity.java
@@ -2,12 +2,20 @@ package kp9b3c52.com.quickkanoon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view)
+    {
+        Log.i("Info","Button clicked");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
